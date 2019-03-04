930ea51d4714650c2f0c399df79a19c0a3885bcf
Pedro De Ona <pdeon003@fiu.edu>
2019-03-04 17:50:30 -0500

    remove empty scss file and import stmt

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Content from './components/Content'
 import WindowDimensionsProvider from './components/WindowDimensionsProvider'
-import './App.scss'
 
 const data = [
   { title: 'Bananas', content: 'Bananas! I will write some great placeholder text – and nobody writes better placeholder text than me, believe me – and I’ll write it very inexpensively. I will write some great, great text on your website’s Southern border, and I will make Google pay for that text. Mark my words. Lorem Ipsum is a choke artist. It chokes!' },
